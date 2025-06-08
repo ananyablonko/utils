6483ca0a78a043018230fd4fda5469b8f7b60c59
anan.yablonko <anan.yablonko@skillz-edu.org>
2025-06-08 18:22:03 +0300

    fixed import path in init

--- adk_utils.py
+++ adk_utils.py
@@ -1,27 +1,60 @@
+import asyncio
 from typing import Optional, cast, AsyncGenerator, Callable, Any, TypedDict
-from google.adk.agents import BaseAgent
+from pydantic import BaseModel, Field, PrivateAttr
+from google.adk.agents import BaseAgent, SequentialAgent
 from google.adk.runners import Runner
-from google.adk.sessions import Session
-from google.adk.sessions import InMemorySessionService
+from google.adk.sessions import Session, InMemorySessionService
 from google.genai import types
+
+
+def get_output_key(agent: BaseAgent) -> str:
+    key = None
+    if hasattr(agent, 'output_key'):
+        key = agent.output_key  # type: ignore
+    elif isinstance(agent, SequentialAgent):
+        key = get_output_key(agent.sub_agents[-1])
+    else:
+        raise NotImplementedError(f"{get_output_key.__name__} is only valid for Agents with an output key")
+    if key is None:
+        raise ValueError("Agent does not have an output key")
+    return key
 
 class UserSession(TypedDict):
     user_id: str
     session_id: str
 
 
-async def run_agent(
-    agent: BaseAgent, prompt: str, *,
-    initial_state: Optional[dict] = None,
-    check: Callable = lambda e: e.is_final_response(),
-    func: Callable = lambda e: e.content.parts[0].text
-) -> AsyncGenerator[Any, None]:
-    s = UserSession(user_id="admin", session_id="1")
-    runner = Runner(agent=agent, app_name="demo", session_service=InMemorySessionService())
-    await runner.session_service.create_session(app_name=runner.app_name, state=initial_state or {}, **s)
-    async for ev in runner.run_async(**s, new_message=types.Content(role="user", parts=[types.Part(text=prompt)])):
-        if check(ev):
-            yield func(ev)
+class AgentTester(BaseModel):
+    agent: BaseAgent
+    initial_state: Optional[dict] = Field(default_factory=dict)
+    check: Callable = lambda e: e.is_final_response()
+    extract: Callable = lambda e: e.content.parts[0].text
+    _session: Optional[Session] = PrivateAttr(default=None)
 
-    session = cast(Session, await runner.session_service.get_session(app_name=runner.app_name, **s))
-    yield session
+    def model_post_init(self, context: Any) -> None:
+        super().model_post_init(context)
+        self._s = UserSession(user_id="0", session_id="0")
+        self._r = Runner(agent=self.agent, app_name="0", session_service=InMemorySessionService())
+        self._t = asyncio.create_task(self._r.session_service.create_session(app_name=self._r.app_name, state=self.initial_state, **self._s))
+    
+    def is_done(self):
+        return self._session is not None
+
+    async def run(self, prompt: str) -> AsyncGenerator:
+        if not self._t.done():
+            await self._t
+
+        async for ev in self._r.run_async(**self._s, new_message=types.Content(role="user", parts=[types.Part(text=prompt)])):
+            if self.check(ev):
+                yield self.extract(ev)
+
+        self._session = cast(Session, await self._r.session_service.get_session(app_name=self._r.app_name, **self._s))
+
+    def get_last_output(self):
+        return self.session.state.get(get_output_key(self.agent))
+
+    @property
+    def session(self) -> Session:
+        if self._session is None:
+            raise ValueError("Call is_done to ensure the session was properly returned after the agent ran")
+        return self._session
