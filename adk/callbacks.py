eeff810737f488c87cda220158741df85a915c05
anan.yablonko <anan.yablonko@skillz-edu.org>
2025-06-23 02:33:31 +0300

    option to skip if key exists

--- adk/callbacks.py
+++ adk/callbacks.py
@@ -3,12 +3,12 @@ from google.adk.agents.callback_context import CallbackContext
 from google.genai import types
 
 
-def should_run_agent(keys: list[str], callback_context: CallbackContext) -> Optional[types.Content]:
+def should_run_agent(keys: list[str], callback_context: CallbackContext, inverse: bool = False) -> Optional[types.Content]:
     f"""
     USAGE
     -----
     before_agent_callback=functools.partial({should_run_agent.__name__}, key=expected_key_to_be_in_session)
     """
     for key in keys:
-        if key not in callback_context.state:
+        if (not inverse) ^ (key in callback_context.state):
             return types.Content(role="model", parts=[types.Part(text=f"Agent {callback_context.agent_name} skipped: {key} not in state!")])
