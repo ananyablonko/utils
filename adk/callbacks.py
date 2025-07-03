27a941c7024815a2b5ce02dfe32de13371ac0d63
anan.yablonko <anan.yablonko@skillz-edu.org>
2025-07-03 17:17:16 +0300

    cosmetic changes

--- adk/callbacks.py
+++ adk/callbacks.py
@@ -10,13 +10,17 @@ from google.genai import types
 def should_run_agent(
     callback_context: CallbackContext, *, prerequisites: Optional[list[str | Callable[[CallbackContext], str]]] = None, cached: Optional[list[str | Callable[[CallbackContext], str]]] = None
 ) -> Optional[types.Content]:
-    f"""
+    """
     USAGE
     -----
-    before_agent_callback=functools.partial({should_run_agent.__name__}, key=expected_key_to_be_in_session)
+    before_agent_callback=functools.partial(should_run_agent, key=expected_key_to_be_in_session)
+
+    RETURNS
+    -------
+    Content with a message if the agent should NOT run, None otherwise.
     """
-    extracted_prereqs: list[str] = [func_or_str(callback_context) if isinstance(func_or_str, Callable) else func_or_str for func_or_str in prerequisites] if prerequisites else []
-    extracted_cached: list[str] = [func_or_str(callback_context) if isinstance(func_or_str, Callable) else func_or_str for func_or_str in cached] if cached else []
+    extracted_prereqs: list[str] = [x(callback_context) if isinstance(x, Callable) else x for x in prerequisites] if prerequisites else []
+    extracted_cached: list[str] = [x(callback_context) if isinstance(x, Callable) else x for x in cached] if cached else []
 
     if cached and all(key in callback_context.state for key in extracted_cached):
         return types.Content(role="model", parts=[types.Part(text=f"Agent {callback_context.agent_name} skipped: {cached} already in state!")])
@@ -35,7 +39,6 @@ def purge_request(callback_context: CallbackContext, llm_request: LlmRequest, *,
                   exclude_pattern: Optional[str] = None
                   ) -> Optional[LlmResponse]:
     
-    llm_request.contents[:] = [c for c in llm_request.contents if
-                               (include_pattern is None or re.search(include_pattern, str(c)))
-                               and
-                               (exclude_pattern is None or not re.search(exclude_pattern, str(c)))]
+    llm_request.contents[:] = [c for c in llm_request.contents if True
+                               and (include_pattern is None or re.search(include_pattern, str(c)))
+                               and (exclude_pattern is None or not re.search(exclude_pattern, str(c)))]
